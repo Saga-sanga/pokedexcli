576627fdc2bc6238a33ea860c3e479ad0d564146
Saga-sanga <vulcan248@gmail.com>
2025-06-06 09:21:05 +0000

    refactor: command Inspect

--- command_inspect.go
+++ command_inspect.go
@@ -11,21 +11,21 @@ func commandInspect(cfg *config, args ...string) error {
 	}
 
 	name := args[0]
-	if val, ok := cfg.caughtPokemon[name]; ok {
-		fmt.Printf("Name: %v\n", val.Name)
-		fmt.Printf("Height: %v\n", val.Height)
-		fmt.Printf("Weight: %v\n", val.Weight)
+	if pokemon, ok := cfg.caughtPokemon[name]; ok {
+		fmt.Printf("Name: %v\n", pokemon.Name)
+		fmt.Printf("Height: %v\n", pokemon.Height)
+		fmt.Printf("Weight: %v\n", pokemon.Weight)
 		fmt.Println("Stats:")
-		for _, stat := range val.Stats {
+		for _, stat := range pokemon.Stats {
 			fmt.Printf("  -%v: %v\n", stat.Stat.Name, stat.BaseStat)
 		}
 		fmt.Println("Types:")
-		for _, pokeType := range val.Types {
+		for _, pokeType := range pokemon.Types {
 			fmt.Printf("  - %v\n", pokeType.Type.Name)
 		}
 
 	} else {
-		fmt.Println("you have not caught that pokemon")
+		return errors.New("you have not caught that pokemon")
 	}
 
 	return nil
